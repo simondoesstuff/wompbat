fb51f934b1e7c090f4fe91aa152bda6b301f3c68
Simon Walker <simon@simonwalker.tech>
2026-09-05 03:55:46 -0600

    chore: add snakemake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,28 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     utils.url = "github:numtide/flake-utils";
   };
   outputs =
     {
       self,
       nixpkgs,
+      nixpkgs-stable,
       utils,
     }:
     utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        stable = nixpkgs-stable.legacyPackages.${system};
       in
       {
         devShell = pkgs.mkShell {
           packages = with pkgs; [
             bun
             just
+            stable.snakemake
           ];
         };
       }
